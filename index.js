0a4d96b82cc1b7913f56d43462958575d045f69c
nichoth <nichoth@gmail.com>
2016-07-09 15:07:31 -0700

    standard and docs

--- index.js
+++ index.js
@@ -18,38 +18,3 @@ module.exports = function onRaf (fn) {
 
   return throttled
 }
-
-
-
-// var throttled = onRaf(function () {
-//   console.log('test')
-// });
-
-
-
-
-
-// function onScroll() {
-//   latestKnownScrollY = window.scrollY;
-//   requestTick();
-// }
-
-// function requestTick() {
-//   if(!ticking) {
-//     requestAnimationFrame(update);
-//   }
-//   ticking = true;
-// }
-
-// function update() {
-//   // reset the tick so we can
-//   // capture the next onScroll
-//   ticking = false;
-
-//   var currentScrollY = latestKnownScrollY;
-
-//   // read offset of DOM elements
-//   // and compare to the currentScrollY value
-//   // then apply some CSS classes
-//   // to the visible items
-// }
--- readme.md
+++ readme.md
@@ -1,7 +1,12 @@
 # raf listen
 
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
 Call a function at most once per animation frame.
 
+*Featuring:*
+* commonjs
+* no es6
 
 ## install
 
@@ -11,8 +16,7 @@ Call a function at most once per animation frame.
 
 ```js
 var throttle = require('raf-listen')
-
-// polyfill raf
+// polyfill raf if you want
 require('raf').polyfill()
 
 setInterval(function () {
